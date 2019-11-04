b77c6b4f71366ff4d897ea7f1578db70fad33efe
S Merchant <sohailsmerchant@gmail.com>
2019-11-04 19:46:09 +0000

    description on top

--- js/public.js
+++ js/public.js
@@ -189,7 +189,7 @@ $(document).ready(function () {
                     }
                     else {
 
-                        return defaultLink + "<strong>Raise an issue/report: </strong> <br/>" + opentag + changeUri + textQuality + completedText + inProgress + endtag
+                        return defaultLink + "<strong>Raise an issue/report: </strong> <br/>" + opentag + changeUri + textQuality + endtag
                     }
                     //return '<a href="' + data + '" target="_blank">Read the full text</a>' + "<span class='bugspan'> <a href ='https://github.com/OpenITI/Annotation/issues/new?assignees=&labels=text+quality&template=text-quality-issue-.md&title=" + fullbookuri + "' target=_blank title='Full Text Issue - raise issue on GitHub'> <i class='fas fa-bug bug'></i></a></span>";
                 }
@@ -228,19 +228,6 @@ $(document).ready(function () {
 
     }
 
-    //myObj = fetch1(myData);
-    //console.log(JSON.stringify(myObj.rowCount));
-
-    //console.log(myData);
-
-    //console.log(Object.keys(myData))
-    //console.log('p:',p['rowCount'])
-
-    // console.log("outside:  ", return_first)
-    // console.log(totalRecords)
-
-    // console.log(totalRecords)
-    // $('#total').html(totalRecords)
 
 
 });
